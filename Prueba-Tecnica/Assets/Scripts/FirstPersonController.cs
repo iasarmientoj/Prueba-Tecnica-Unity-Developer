48ca71dd54569a7a04357e40e3801d52b835dab6
novatransmedia <41652611+iasarmientoj@users.noreply.github.com>
2024-12-01 01:12:43 -0500

    reset position player

--- Prueba-Tecnica/Assets/Scripts/FirstPersonController.cs
+++ Prueba-Tecnica/Assets/Scripts/FirstPersonController.cs
@@ -6,9 +6,9 @@ using UnityEngine.EventSystems;
 
 public class FirstPersonController : MonoBehaviour
 {
-    public bool CanMove{ get; private set; } = true;
+    public bool CanMove { get; private set; } = true;
     private bool IsSprinting => canSprint && Input.GetKey(sprintKey);
-    private bool ShouldJump =>  Input.GetKey(jumpKey) && characterController.isGrounded;
+    private bool ShouldJump => Input.GetKey(jumpKey) && characterController.isGrounded;
     private bool ShouldCrouch => Input.GetKeyDown(crouchKey) && !duringCrouchAnimation && characterController.isGrounded;
 
     [Header("Functional Options")]
@@ -45,7 +45,7 @@ public class FirstPersonController : MonoBehaviour
     [SerializeField] private float crouchHeight = 0.5f;
     [SerializeField] private float standingHeight = 2f;
     [SerializeField] private float timeToCrouch = 0.25f;
-    [SerializeField] private Vector3 crouchingCenter = new Vector3(0, 0.5f,0);
+    [SerializeField] private Vector3 crouchingCenter = new Vector3(0, 0.5f, 0);
     [SerializeField] private Vector3 standingCenter = new Vector3(0, 0, 0);
     private bool isCrouching;
     private bool duringCrouchAnimation;
@@ -129,6 +129,13 @@ public class FirstPersonController : MonoBehaviour
         }
 
     }
+
+    public void ResetPosition()
+    {
+        transform.position = new Vector3(6, 1, 6);
+    }
+
+
     private void HandleMovementInput()
     {
         currentInput = new Vector2((isCrouching ? crouchSpeed : IsSprinting ? sprintSpeed : walkSpeed) * Input.GetAxis("Vertical"), (isCrouching ? crouchSpeed : IsSprinting ? sprintSpeed : walkSpeed) * Input.GetAxis("Horizontal"));
